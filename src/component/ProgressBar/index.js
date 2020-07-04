84ac6c8d117f0acdc3aeef6cad9fd6d322121f78
Thau26 <thauanyaraujoalves@hotmail.com>
2020-07-04 16:45:30 -0300

    add componente barra de progresso

---
+++ src/component/ProgressBar/index.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react';
+import { Animated, Easing, SafeAreaView, StyleSheet } from 'react-native';
+import styles from './styles';
+
+
+export default function ProgressBar() {
+    
+    const [offsetX] = useState(new Animated.Value(-400));
+    
+    const translate = Animated.timing(offsetX, {
+      toValue: 0,
+      duration: 1000,
+      easing: Easing.inOut(Easing.linear),
+      useNativeDriver: true,
+    });
+    const reset = Animated.timing(offsetX, {
+      toValue: -430,
+      duration: 0,
+      useNativeDriver: true,
+    });
+    const animation = Animated.sequence([translate, reset]);
+    useEffect(() => {
+      Animated.loop(animation).start();
+  
+      // Substituir esse setTimeout por uma chamada http ou qualquer outra chamada de serviço.
+      setTimeout(() => {
+        console.log('Chamar serviço');
+      }, 4000);
+    }, [animation]);
+    const transform = {transform: [{translateX: offsetX}]};
+    
+    return (
+      <SafeAreaView>
+        <Animated.View style={styles.syncProgressBarContainer}>
+          <Animated.View style={[transform, styles.syncProgressBar]} />
+          <Animated.View style={[transform, styles.syncProgressBar]} />
+          <Animated.View style={[transform, styles.syncProgressBar]} />
+          <Animated.View style={[transform, styles.syncProgressBar]} />
+        </Animated.View>
+      </SafeAreaView>
+    );
+  };
+  
---
+++ src/component/ProgressBar/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    
+    syncProgressBarContainer: {
+      flexDirection: 'row',
+    },
+    syncProgressBar: {
+      height: 4,
+      marginHorizontal: 10,
+      width: 200,
+      backgroundColor: '#0000ff',
+    },
+  });
+  
